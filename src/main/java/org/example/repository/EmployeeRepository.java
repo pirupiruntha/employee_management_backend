9e51a5133d1897d0c59ca32aeb95bd71214629a2
pirupiruntha <spiruntha1116@gmail.com>
2023-01-26 13:24:24 +0530

    EmployeeRepository extends with MongoRepository

--- src/main/java/org/example/repository/EmployeeRepository.java
+++ src/main/java/org/example/repository/EmployeeRepository.java
@@ -1,4 +1,12 @@
 package org.example.repository;
 
-public class EmployeeRepository {
+import org.example.model.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
+    Optional<Employee> findById(String id);
 }
