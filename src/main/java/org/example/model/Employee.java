fa84117040b3da32313e5dc4e10b1e2ab7d413be
pirupiruntha <spiruntha1116@gmail.com>
2023-01-26 13:29:15 +0530

    add needed annotations and variable declaration

--- src/main/java/org/example/model/Employee.java
+++ src/main/java/org/example/model/Employee.java
@@ -1,4 +1,22 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "employee_details")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Employee {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String dob;
+    private Double salary;
+
 }
